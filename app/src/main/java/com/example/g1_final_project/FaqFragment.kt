5cd94b3163cef185321ba0488cfa6c347e6a509f
inbguzman <qinbguzman@tip.edu.ph>
2022-12-02 14:48:05 +0800

    First Commit

---
+++ app/src/main/java/com/example/g1_final_project/FaqFragment.kt
@@ -0,0 +1,21 @@
+package com.example.g1_final_project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.ui.AppBarConfiguration
+
+class FaqFragment : Fragment() {
+    lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_faq, container, false)
+    }
+
+}
