4b393d6cae866c0e3e2d5d54184150de2fdd1766
inbguzman <qinbguzman@tip.edu.ph>
2022-12-05 21:56:52 +0800

    Login and Register UI

--- app/src/main/java/com/example/g1_final_project/LoginFragment.kt
+++ app/src/main/java/com/example/g1_final_project/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.g1_final_project.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
@@ -13,6 +15,17 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val binding = FragmentLoginBinding.bind(view)
+
+        binding.btnCreate.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/g1_final_project/RegisterFragment.kt
+++ app/src/main/java/com/example/g1_final_project/RegisterFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.g1_final_project.databinding.FragmentLoginBinding
+import com.example.g1_final_project.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
 
@@ -13,7 +16,18 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+        val binding = FragmentRegisterBinding.bind(view)
+
+        binding.btnBackLogin.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+
+        binding.btnSignup.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+
+        return binding.root
     }
 
 }
