cb31a5553225e09a1fd1e69331111cb1529e43a1
inbguzman <qinbguzman@tip.edu.ph>
2022-12-09 13:33:43 +0800

    All UI Done

--- app/src/main/java/com/example/g1_final_project/MainActivity.kt
+++ app/src/main/java/com/example/g1_final_project/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.g1_final_project.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var appBarConfiguration: AppBarConfiguration
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
